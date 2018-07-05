25ed45fa8255131161b920120a9c87b8b9c780a0
RJ Bergeron <rbergero@gmail.com>
2018-07-05 13:34:24 -0400

    publish to correct image, nopush supports

--- publish.sh
+++ publish.sh
@@ -6,11 +6,11 @@ for img in $(docker images "build/*" --format "{{.Repository}}:{{.Tag}}") ; do
   dest="${img#build/}"
   dest="${dest%:latest}"
   case "${dest}" in [0-9]*) continue ;; esac
-  docker tag "${img}" "${DOCKER_SINK}/xenial-installer:${dest}"
-  docker tag "${img}" "${DOCKER_SINK}/xenial-installer:${dest}.${ts}"
+  docker tag "${img}" "${DOCKER_SINK}/xenial-hypervisor:latest"
+  docker tag "${img}" "${DOCKER_SINK}/xenial-hypervisor:latest.${ts}"
   [ "${NOPUSH}" ] || {
-    docker push "${DOCKER_SINK}/xenial-installer:${dest}"
-    docker push "${DOCKER_SINK}/xenial-installer:${dest}.${ts}"
+    docker push "${DOCKER_SINK}/xenial-hypervisor:latest"
+    docker push "${DOCKER_SINK}/xenial-hypervisor:latest.${ts}"
   }
 done
 
